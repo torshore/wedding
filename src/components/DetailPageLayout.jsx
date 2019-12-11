2bd7c6651276858895f14f28379d0d5b43aca28c
Tory Shoreman <tory.shoreman@beamworks.io>
2019-12-11 17:06:18 -0500

    Add detail page layout

---
+++ src/components/DetailPageLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/DetailPageLayout.scss';
+
+const DetailPageLayout = ({children}) => {
+    return <div className="detail-page-layout">
+        <div className="_content">
+            {children}
+        </div>
+    </div>
+}
+
+export default DetailPageLayout;
