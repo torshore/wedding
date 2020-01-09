be1a223fa591539b61ef2639d23f62ef08e5d41b
Tory Shoreman <tory.shoreman@beamworks.io>
2020-01-09 18:38:05 -0500

    Add hotel booking page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ import '../styles/NavBar.scss';
 const NavBar = () => {
     return <div className="nav-bar">
         <Link to="/wedding/">THE WEDDING</Link>
-        <div>HOTELS</div>
+        <Link to="/hotels/">HOTELS</Link>
         <div>REGISTRY</div>
         <div>RSVP</div>
         <div>CONTACT US</div>
