3353dec37bc31cc1152ac75211c5bd806aa7b3d3
Tory Shoreman <tory.shoreman@beamworks.io>
2020-07-22 16:10:42 -0400

    Date changes

--- src/pages/wedding.js
+++ src/pages/wedding.js
@@ -17,7 +17,7 @@ export default () => (
             <div className="_details">
                 <div className="_column">
                     <span className="_cursive">when</span>
-                    <span className="_outline">Sept 20th, 2020</span>
+                    <span className="_outline">Oct 22, 2021</span>
                     <span className="_outline">3:00 PM</span>
                     <span>COCKTAIL HOUR, DINNER & DANCING TO FOLLOW</span>
                 </div>
@@ -32,7 +32,6 @@ export default () => (
             <div className="_info">
                 <span className="_title">stuff to know</span>
                 <ul>
-                    <li>If large gatherings are not allowed in September, we will send out another email to all guests with an update sometime in the summer.</li>
                     <li>If you need to change your RSVP due to the pandemic, simply rsvp again with updated information.</li>
                     <li>If you are staying at Ivey Spencer, a shuttle will take you to and from the venue.  You will find the pick-up times here closer to the wedding date!</li>
                     <li>Bellamere asks that our guests sing a song instead of clinking glasses.  Get your cheesy, karaoke classics ready!</li>
